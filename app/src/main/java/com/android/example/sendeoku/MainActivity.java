2ab156e51d036a0cb475e722a55026f1473e24a0
Umut <umutuz7@gmail.com>
2018-03-17 12:50:47 +0300

    Add Book Fragment with dummy recyclerview

--- app/src/main/java/com/android/example/sendeoku/MainActivity.java
+++ app/src/main/java/com/android/example/sendeoku/MainActivity.java
@@ -3,11 +3,18 @@ package com.android.example.sendeoku;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.example.sendeoku.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements BookFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
